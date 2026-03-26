2cd9b486db7bea4baf19a8de160c188533470089
amyep1 <amy.patterson@urbanarts.org>
2026-03-25 23:15:56 -0400

    Update curriculum.js

--- api/curriculum.js
+++ api/curriculum.js
@@ -1,11 +1,13 @@
 export default async function handler(req, res) {
   // Allow requests from any origin (fixes CORS)
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Max-Age", "86400");
 
   if (req.method === "OPTIONS") {
-    return res.status(200).end();
+    res.status(200).end();
+    return;
   }
 
   const PROXY_URL = "https://script.google.com/macros/s/AKfycbzZIOII2P4k94PVMO3yQ0gGIFkr5FS8y0bUGm7j24JSctPBNKJgTFSMK0LMK8lHL1z64Q/exec";
